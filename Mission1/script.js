713f1463eeb5d4c2bd4f2e0d063d515f39703bb9
kayrito5 <batigoal53@gmail.com>
2022-12-02 14:35:55 +0100

    Mission 1

---
+++ Mission1/script.js
@@ -0,0 +1,7 @@
+let toto = "Toto";
+const ENFANTS = 3;
+let JOUETS = ["poupée", "trains", "voitures"];
+
+console.log(toto + " à " + ENFANTS + " enfants qui aiment les " + JOUETS[2]);
+
+console.log(`Les ${ENFANTS} enfants de ${toto} détestent les ${JOUETS[1]}`);
